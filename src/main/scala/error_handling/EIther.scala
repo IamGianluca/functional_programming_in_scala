418eb37eb612090e265004441b203802faae3c7e
Gianluca Rossi <grossi@quantcast.com>
2015-07-07 22:05:28 +0100

    Add traverse and sequence functions

--- src/main/scala/error_handling/EIther.scala
+++ src/main/scala/error_handling/EIther.scala
@@ -24,9 +24,22 @@ sealed trait Either[+E, +A] {
       a <- this
       b1 <- b
     } yield f(a, b1)
+
+  /** Map over a list using a function that might fail, returning the first error
+    *   that is encountered, if there is one */
+  def traverse[E, A, B](as: List[A])(f: A => Either[E, B]): Either[E, List[B]] =
+    as match {
+      case Nil => Right(Nil)
+      case h :: t => (f(h) map2 traverse(t)(f))(_ :: _)
+
+    }
+
+  /** Combines a list of Either into one Either containing a list of all
+    *   the values in the original list. If the original list contains
+    *   an error, the result of the function should include the error */
+  def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] =
+    traverse(es)(x => x)
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
-
-
