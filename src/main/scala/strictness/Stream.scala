ff0fd962b9e0c3cef65f1751ab3c83af360ff800
Gianluca Rossi <grossi@quantcast.com>
2015-07-09 00:31:31 +0100

    Add functions exists, foldRight and forAll. Add implementations of takeWhile and exists using foldRight.

--- src/main/scala/strictness/Stream.scala
+++ src/main/scala/strictness/Stream.scala
@@ -10,6 +10,10 @@ sealed trait Stream[+A] {
       case Cons(h, t) => Some(h())
     }
 
+  /** Write headOption using foldRight */
+  def headOption2: Option[A] =
+    ???
+
   /** Convert a Stream to a List, which will force its evaluation and let you look at it in the REPL */
   def toList: List[A] =
     this match {
@@ -38,6 +42,32 @@ sealed trait Stream[+A] {
       case Cons(h, t) if f(h()) => cons(h(), t() takeWhile f)
       case _ => empty
     }
+
+  /** Write takeWhite using foldRight */
+  def takeWhile2(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((h, t) => if (f(h)) cons(h, t) else empty)
+
+  /** Check whether an element matching a Boolean function exists in this stream */
+  def exists(p: A => Boolean): Boolean =
+    this match {
+      case Cons(h, t) => p(h()) || t().exists(p)
+      case _ => false
+    }
+
+  /** Write exists in terms of foldRight */
+  def exists2(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
+
+  /** Implement a general recursion */
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  /** Check that all elements in the Stream match a given predicate */
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a))
 }
 
 case object Empty extends Stream[Nothing]
