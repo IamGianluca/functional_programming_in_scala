e1eeffff900e8da07e89a445762186108be7ae16
Gianluca Rossi <grossi@quantcast.com>
2015-07-08 20:33:15 +0100

    Create a toList method to convert a Stream to a List

--- src/main/scala/strictness/Stream.scala
+++ src/main/scala/strictness/Stream.scala
@@ -1,18 +1,38 @@
 package strictness
 
-sealed trait Stream[+A]
+sealed trait Stream[+A] {
+  /** Optionally extract the head of a stream */
+  def headOption: Option[A] =
+    this match {
+      case Empty =>  None
+      case Cons(h, t) => Some(h())
+    }
+
+  /** Convert a Stream to a List, which will force its
+    *   evaluation and let you look at it in the REPL */
+  def toList: List[A] = this match {
+    case Cons(h,t) => h() :: t().toList
+    case _ => List()
+  }
+}
+
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
+  /** Smart constructor for creating a nonempty stream */
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
     Cons(() => head, () => tail)
   }
 
+  /** Smart constructor for creating an empty stream of a particular
+    *   type */
   def empty[A]: Stream[A] = Empty
 
+  /** Convenient variable-argument method for constructing a Stream
+    *   from multiple elements */
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 }
