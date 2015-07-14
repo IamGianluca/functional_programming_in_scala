68e0ddfe98bf93136192a7921c71fedd7c8347be
Gianluca Rossi <grossi@quantcast.com>
2015-07-14 08:34:35 +0100

    Add unfold to class Stream

--- src/main/scala/strictness/Stream.scala
+++ src/main/scala/strictness/Stream.scala
@@ -107,9 +107,13 @@ sealed trait Stream[+A] {
     fib()
   }
 
-  /** General stream building function */
+  /** General stream building function
+    * Source: https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/laziness/Stream.scala */
   def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
-    ???
+    f(z) match {
+      case Some((h, t)) => cons(h, unfold(t)(f))
+      case None => empty
+    }
 }
 
 case object Empty extends Stream[Nothing]
