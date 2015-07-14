f344533a07cd99d01ce02b5561824c8a4df24ebe
Gianluca Rossi <grossi@quantcast.com>
2015-07-14 13:36:51 +0100

    Fix takeViaUnfold

--- src/main/scala/strictness/Stream.scala
+++ src/main/scala/strictness/Stream.scala
@@ -30,9 +30,9 @@ sealed trait Stream[+A] {
     }
 
   def takeViaUnfold(n: Int): Stream[A] =
-    unfold(this, n) {
+    unfold((this, n)) {
       case (Cons(h, t), 1) => Some((h(), (empty, 0)))
-      case (Cons(h, t), n) => Some((h(), (t(), n - 1)))
+      case (Cons(h, t), n) if n > 1 => Some((h(), (t(), n - 1)))
       case _ => None
     }
 
