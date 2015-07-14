2fc31a6ff3257dcfde4c90b83afc82ca52a2c33f
Gianluca Rossi <grossi@quantcast.com>
2015-07-14 22:09:15 +0100

    Add scanRight and hasSubsequence to Stream class

--- src/main/scala/strictness/Stream.scala
+++ src/main/scala/strictness/Stream.scala
@@ -126,6 +126,25 @@ sealed trait Stream[+A] {
       case s => Some((s, s drop 1))
     } append Stream(empty)
 
+  /** Generalise tails. This function is like a foldRight that returns a sstream of the intermediate results
+    * Source: https://github.com/fpinscala/fpinscala/blob/master/answers/src/main/scala/fpinscala/laziness/Stream.scala
+    *
+    * The function can't be implemented using `unfold`, since `unfold` generates elements of the `Stream` from left to
+    *   right. It can be implemented using `foldRight` though. The implementation is just a `foldRight` that keeps the
+    *   accumulated value and the stream of intermediate results, which we `cons` onto during each iteration. When
+    *   writing folds, it's common to have more state in the fold than is needed to compute the result. Here, we simply
+    *   extract the accumulated list once finished */
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, p0) => {
+      // p0 is passed by-name and used in by-name args in f and cons. So use lazy val to ensure only one evaluation...
+      lazy val p1 = p0
+      val b2 = f(a, p1._1)
+      (b2, cons(b2, p1._2))
+    })._2
+
+  def hasSubsequence[B](s: Stream[B]): Boolean =
+    tails exists (_ startsWith s)
+
   def filter(f: A => Boolean): Stream[A] =
     foldRight(empty[A])((h,t) => if (f(h)) cons(h, t) else t)
 
